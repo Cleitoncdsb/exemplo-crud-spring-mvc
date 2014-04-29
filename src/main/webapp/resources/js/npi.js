3b108b8acc08b4fa60489c9ad52ee5acbc81a08b
leoneljunior <junior.hpj@hotmail.com>
2014-04-29 17:00:35 -0300

    Adicionado método submit do botão da tela modal. (Ainda não funcionando)

--- src/main/webapp/resources/js/npi.js
+++ src/main/webapp/resources/js/npi.js
@@ -52,3 +52,22 @@ function excluir(uri, row) {
 		}
 	});
 };
+
+//this is the id of the form 
+$("#idForm").submit(function() { 
+	var url = "path/to/your/script.php"; 
+	var method = $("#idform").Attr('method');
+
+	// the script where you handle the form input. 
+	$.ajax({ 
+	type: method, 
+	url: url, 
+	data: $("#idForm").serialize(), 
+	// serializes the form's elements. 
+	success: function(data) { 
+		alert(data); // show response from the php script. 
+	} }); 
+		return false; // avoid to execute the actual submit of the form. 
+});
+	
+
