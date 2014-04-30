d740244996081d1adda7b000cbaa1635f301749a
pedrosiufc <pedrosiufc@gmail.com>
2014-04-30 16:26:35 -0300

    Mensages de alerta.

--- src/main/webapp/resources/js/npi.js
+++ src/main/webapp/resources/js/npi.js
@@ -43,8 +43,20 @@ function excluir(idTable ,uri, row) {
 		type : "DELETE",
 		url : uri,
 		success : function(data) {
-			console.log(data);
-			dt.fnDeleteRow($(row).parents('tr')[0]);
+			if(data=="ok"){
+			  $('#mensagens').removeClass('alert-danger');
+			  $('#mensagens').addClass('alert-success');
+			  $('#mensagens').show();	
+			  $('#mensagens').text("Remoção Feita com Sucesso");
+		      $('#mensagens').fadeOut(4000);
+			  dt.fnDeleteRow($(row).parents('tr')[0]);
+			}else{
+				$('#mensagens').removeClass('alert-success');
+				$('#mensagens').addClass('alert-danger');
+				$('#mensagens').show();	
+				$('#mensagens').text("Reportando Erros");
+			      
+			}
 		}
 	});
 };
