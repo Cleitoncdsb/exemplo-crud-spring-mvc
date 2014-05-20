5789a943ea6beae6d8a242532718982054cd2831
Adeilsonara <adeilsonaragao@hotmail.com>
2014-05-20 14:42:13 -0300

    personalização do applicationContext

--- src/test/java/br/ufc/quixada/npi/service/ContatoServiceTest.java
+++ src/test/java/br/ufc/quixada/npi/service/ContatoServiceTest.java
@@ -3,14 +3,18 @@ package br.ufc.quixada.npi.service;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.ufc.quixada.npi.model.Contato;
 
-public class ContatoServiceTest {
+public abstract class ContatoServiceTest {
 	
+	@Autowired
 	protected ContatoService contatoService;
 	
 	@Test
+	@Transactional
 	public void testSalvar() {
 		Contato contato = new Contato();
 		contato.setNome("José");
@@ -23,8 +27,14 @@ public class ContatoServiceTest {
 	}
 
 	@Test
-	public void testAtualizar() {
-		
+	@Transactional
+	public void testAtualizar() throws Exception {
+		Contato novo = this.contatoService.findById(1);
+		String velho = novo.getNome();
+		novo.setNome(velho + "Modf");
+		this.contatoService.salvar(novo);
+		novo = this.contatoService.findById(1);
+		assertEquals(velho + "Modf", novo.getNome());
 	}
 
 	@Test
