a88d03baa4f14069256efd2510f7cb209e448217
claumatias <claudiomatias.3c@gmail.com>
2014-05-20 16:34:37 -0300

    * Configuração no pom.xml para as dependencias de testes; * Atualização do applicationContextTest * Novas Classes: UsuarioServiceJdbcTest / UsuarioServiceJpaTest e UsuarioServiceSpringDataTest

---
+++ src/test/java/br/ufc/quixada/npi/service/UsuarioServiceJdbcTest.java
@@ -0,0 +1,13 @@
+package br.ufc.quixada.npi.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(locations = {"classpath:spring/applicationContextTest.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("jdbc")
+public class UsuarioServiceJdbcTest extends AbstractUsuarioServiceTest{	
+
+}
---
+++ src/test/java/br/ufc/quixada/npi/service/UsuarioServiceJpaTest.java
@@ -0,0 +1,14 @@
+package br.ufc.quixada.npi.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(locations = {"classpath:spring/applicationContextTest.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("jpa")
+public class UsuarioServiceJpaTest extends AbstractUsuarioServiceTest {
+
+	
+}
