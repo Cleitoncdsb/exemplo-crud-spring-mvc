beaa11da8862b519e670011929e1a547aeba4ff1
Adeilsonara <adeilsonaragao@hotmail.com>
2014-05-26 14:22:07 -0300

    Povoamento do BD

--- src/main/resources/db/h2database/populateDB.sql
+++ src/main/resources/db/h2database/populateDB.sql
@@ -20,33 +20,38 @@ INSERT INTO usuario(
 	
 INSERT INTO contato(
 			id, nome, sobrenome, fone, cidade, endereço, email)
-	VALUES (01, 'ana', 'maria' '98586332', 'quixada', 'rua 1, 100', 'anamaria@gmail.com');
+	VALUES (01, 'ana', 'maria','98586332', 'quixada', 'rua 1, 100', 'anamaria@gmail.com');
 
 INSERT INTO contato(
 			id, nome, sobrenome, fone, cidade, endereço, email)
-	VALUES (02, 'maria', 'paula' '45872136', 'fortaleza', 'rua margaridas, 57', 'mariapaula@gmail.com');
+	VALUES (02, 'maria', 'paula', '45872136', 'fortaleza', 'rua margaridas, 57', 'mariapaula@gmail.com');
 
 INSERT INTO contato(
 			id, nome, sobrenome, fone, cidade, endereço, email)
-	VALUES (03, 'marco', 'luis' '45852147', 'quixeramobim', 'rua 503, 25', 'marcoluis@gmail.com');
+	VALUES (03, 'marco', 'luis', '45852147', 'quixeramobim', 'rua 503, 25', 'marcoluis@gmail.com');
 
 INSERT INTO contato(
 			id, nome, sobrenome, fone, cidade, endereço, email)
-	VALUES (04, 'joao', 'lucas' '74581236', 'fortaleza', 'rua nova, 741', 'joaolucas@gmail.com');
+	VALUES (04, 'joao', 'lucas', '74581236', 'fortaleza', 'rua nova, 741', 'joaolucas@gmail.com');
 
 INSERT INTO contato(
 			id, nome, sobrenome, fone, cidade, endereço, email)
-	VALUES (05, 'maria', 'luisa' '98586332', 'quixada', 'rua 15 de outubro, s/n', 'anamaria@gmail.com');
-	
-INSERT INTO papel(
-			id, nome,)
-	VALUES (01, 'admin');
+	VALUES (05, 'maria', 'luisa', '98586332', 'quixada', 'rua 15 de outubro, s/n', 'anamaria@gmail.com');
 	
 INSERT INTO papel(
 			id, nome)
-	VALUES (02, 'servidor');
+	VALUES (01, 'ROLE_USER');
 	
 INSERT INTO papel(
 			id, nome)
-	VALUES (03, 'aluno');
+	VALUES (02, 'ROLE_VET');
+
+INSERT INTO papel_usuario(
+			usuario_id, papel_id)
+	VALUES (01, '01');
+
+INSERT INTO papel_usuario(
+			usuario_id, papel_id)
+	VALUES (01, '02');
+
 	
