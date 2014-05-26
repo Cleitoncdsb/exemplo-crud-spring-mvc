950709d7e6ac9da10e40ee82c25c03fc3ebf7cd0
Ufc <Ufc@200.129.38.173>
2014-05-26 16:43:46 -0300

    Criação das classes de teste da Classe Contato

---
+++ src/test/java/br/ufc/quixada/npi/service/ContatoServiceSpringDataJpaTest.java
@@ -0,0 +1,14 @@
+package br.ufc.quixada.npi.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContatoServiceSpringDataJpaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
