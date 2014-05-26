cdbb8539a4f60e6b257cc19be42c2fa90321b197
claumatias <claudiomatias.3c@gmail.com>
2014-05-26 16:47:15 -0300

    Criação da pasta resources em test e remoção das configurações para o banco de teste e applicationContextTest.xml

--- src/test/resources/db/h2database/populateDB.sql
+++ src/test/resources/db/h2database/populateDB.sql
@@ -1,22 +1,22 @@
 INSERT INTO usuario(
 			id, login, password, habilitado)
-	VALUES (01, 'maria', 'A665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3', true);
+	VALUES (01, 'maria', '123', true);
 	
 INSERT INTO usuario(
 			id, login, password, habilitado)
-	VALUES (02, 'jose', 'B3A8E0E1F9AB1BFE3A36F231F676F78BB30A519D2B21E6C530C0EEE8EBB4A5D0', true);
+	VALUES (02, 'jose', '456', true);
 	
 INSERT INTO usuario(
 			id, login, password, habilitado)
-	VALUES (03, 'ana', '35A9E381B1A27567549B5F8A6F783C167EBF809F1C4D6A9E367240484D8CE281', true);
+	VALUES (03, 'ana', '789', true);
 	
 INSERT INTO usuario(
 			id, login, password, habilitado)
-	VALUES (04, 'joao', '1D28C120568C10E19B9D8ABE8B66D0983FA3D2E11EE7751ACA50F83C6F4A43AA', true);
+	VALUES (04, 'joao', '147', true);
 	
 INSERT INTO usuario(
 			id, login, password, habilitado)
-	VALUES (05, 'carla', 'A30F4EF42176D28F0E2293533C5F532E9C9C5696C68813B35315D17EDC44F6B1', true);
+	VALUES (05, 'joana', '258', true);
 	
 INSERT INTO contato(
 			id, nome, sobrenome, fone, cidade, endereco, email)
